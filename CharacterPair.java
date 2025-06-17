d2a2073608f1c5796c35b8ba31412793534c7f0b
lawadeolokun <146070831+lawadeolokun@users.noreply.github.com>
2025-06-17 15:01:50 +0100

    Delete CharacterPair.java

--- CharacterPair.java
+++
@@ -1,9 +0,0 @@
-public class CharacterPair {
-    char character; // character that is going to be encoded
-    String code;    // Huffman code for binary
-
-    public CharacterPair(char character, String code) {
-        this.character = character;
-        this.code = code;
-    }
-}
